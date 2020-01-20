f09475b3ee030299152482204145eed1e26c3e3c
Tabue <58251936+Tabue@users.noreply.github.com>
2020-01-20 17:50:54 +0300

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -2,5 +2,5 @@
 #This notebook was created in order to accomplish the course
 import pandas as pd
 import numpy as np
-print "Hello captsone project course" 
+print ("Hello captsone project course")
 
