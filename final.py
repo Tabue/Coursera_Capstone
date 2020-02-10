2e396809f55bac8e0695dc9ed9ded981d40d2eae
Tabue <58251936+Tabue@users.noreply.github.com>
2020-02-10 14:48:48 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,1 +1,7 @@
+# this notebook will be used for accomplishment of the final task.
+# Since more and more people are keen on sport, we may want to open new sportgym in torronto, but first we should check whether there are business prerequisites fo this.\
 
+import pandas as pd #this will be used for data wrangling
+import numpy as np
+import requests # maybe we will use oarcing, who knows
+#  with the help of foursquare we will find all gyms and creat clusters, then we will analyze the density of sportgyms in each neighbour and choose the one which has the samllest density (or we will choose another indicator). 
