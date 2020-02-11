cc6152ae1d14b21ff05589471d247adf9ba75089
Tabue <58251936+Tabue@users.noreply.github.com>
2020-02-11 09:17:30 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,7 +1,10 @@
-# this notebook will be used for accomplishment of the final task.
-# Since more and more people are keen on sport, we may want to open new sportgym in torronto, but first we should check whether there are business prerequisites fo this.\
-
-import pandas as pd #this will be used for data wrangling
-import numpy as np
-import requests # maybe we will use oarcing, who knows
-#  with the help of foursquare we will find all gyms and creat clusters, then we will analyze the density of sportgyms in each neighbour and choose the one which has the samllest density (or we will choose another indicator). 
+# In this project we will try to find an optimal location for a sportgym.
+# Since more and more people are keen on sport, we may want to open new sportgym in torronto, but first we should check whether there are business prerequisites fo this.
+#of course there are many gyms still, with the help of foursquare we will try to detect best place for our potential facility, looking at the density of sportgyms in each cluster, and distance from the centr..
+#Key data:
+#number of existing sportgyms in the neighborhood
+#distance of neighborhood from center
+#concept:
+# 1)machine learning will help us to cluster neighbourhoods
+# 2)foursquare to obtain all sportgyms in neighbours
+# 3)geocoding and folium wiull; help us to visualize sportgyms and make it easier to make decision.
